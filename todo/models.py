879ffb4eed21ab500ec376f3cff999d447e43a68
m r <a.m.rasouli.n@gmail.com>
2021-06-18 17:23:50 +0430

    added project and task models, a little change in login template

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Project(models.Model):
+    Project_name = models.CharField(max_length=64)
+    Project_description = models.CharField(max_length=128)
+    Project_isDone = models.BooleanField(default=False)
+    Project_deadline = models.DateField(null=True)
+    Project_submitDate = models.DateTimeField(default=timezone.now)
+
+class Task(models.Model):
+    Task_name = models.CharField(max_length=64)
+    Task_description = models.CharField(max_length=128)
+    Task_isDone = models.BooleanField(default=False)
+    Task_project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    Task_submitDate = models.DateTimeField(default=timezone.now)
+
