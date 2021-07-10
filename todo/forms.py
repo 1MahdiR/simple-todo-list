69709ca2e6f5ff722f25fa5c9dfef338594f33ef
m r <a.m.rasouli.n@gmail.com>
2021-07-10 10:51:25 +0430

    Added todo.forms

--- todo/forms.py
+++ todo/forms.py
@@ -1,11 +1,19 @@
 
 from django import forms
+from bootstrap_datepicker_plus import DatePickerInput
 
 from .models import Task, Project
 
-class AddTaskForm(forms.ModelForm):
+class AddTaskForm(forms.Form):
 
-    class Meta:
-        model = Task
-        fields = ('name', 'description', 'project')
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    doer = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    PROJECTS = [ (item.pk,item.name) for item in Task.objects.all() ]
+    PROJECTS.append((0, "No project"))
+    project = forms.ChoiceField(choices=PROJECTS, widget=forms.Select(attrs={'class':'form-control'}))
 
+class AddProjectForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    deadline = forms.DateField(widget=DatePickerInput(format="%Y/%m/%d"))
