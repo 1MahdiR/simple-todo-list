814cb1d132c290666004b346a64835e4fb79576e
m r <a.m.rasouli.n@gmail.com>
2021-07-10 10:44:43 +0430

    Added user field to todo.models

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
@@ -8,11 +9,14 @@ class Project(models.Model):
     isDone = models.BooleanField(default=False)
     deadline = models.DateField(null=True)
     submitDate = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Task(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=128)
+    doer = models.CharField(max_length=64)
     isDone = models.BooleanField(default=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
     submitDate = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, default=project, on_delete=models.CASCADE)
 
