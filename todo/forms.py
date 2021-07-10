d5138b0a5a2f48d4612dd6b3e72358a8b26e9ce5
m r <a.m.rasouli.n@gmail.com>
2021-07-10 12:53:39 +0430

    Fixed todo.forms

--- todo/forms.py
+++ todo/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-from bootstrap_datepicker_plus import DatePickerInput
+from django.utils import timezone
 
 from .models import Task, Project
 
@@ -9,11 +9,11 @@ class AddTaskForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     doer = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    PROJECTS = [ (item.pk,item.name) for item in Task.objects.all() ]
+    PROJECTS = [ (item.pk,item.name) for item in Project.objects.all() ]
     PROJECTS.append((0, "No project"))
     project = forms.ChoiceField(choices=PROJECTS, widget=forms.Select(attrs={'class':'form-control'}))
 
 class AddProjectForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    deadline = forms.DateField(widget=DatePickerInput(format="%Y/%m/%d"))
+    deadline = forms.DateField(widget=forms.DateInput(attrs={'type':'date', 'class':'form-control', 'value':timezone.now().strftime("%Y/%m/%d")}))
