ed1f66ad4e2d6afabcd0ece4692238cc03c20930
m r <a.m.rasouli.n@gmail.com>
2021-07-12 11:12:20 +0430

    add_project is functional

--- todo/views.py
+++ todo/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Task, Project
-from .forms import AddTaskForm
+from .forms import AddTaskForm, AddProjectForm
 
 # Create your views here.
+@login_required
 def add_task(req):
 
     if req.method == "POST":
-        form = AddTaskForm(data=req.POST)
-        if form.is_valid:
-            form.save()
+        form = AddTaskForm(req.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            project = None
+            if cd['project'] != '0':
+                project = Project.objects.get(pk=cd['project'])
+
+            new_task = Task.objects.create(name=cd['name'], description=cd['description'], doer=cd['doer'],
+                                    isDone=False, project=project, user=req.user)
         return HttpResponseRedirect(reverse("account:dashboard"))
     else:
         form = AddTaskForm()
 
     return render(req, 'todo/add_task.html', {'form':form})
 
+@login_required
+def add_project(req):
+
+    if req.method == "POST":
+        form = AddProjectForm(req.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            new_project = Project.objects.create(name=cd['name'], description=cd['description'], deadline=cd['deadline'], user=req.user)
+        return HttpResponseRedirect(reverse("account:dashboard"))
+    else:
+        form = AddProjectForm()
+
+    return render(req, 'todo/add_project.html', {'form':form})
